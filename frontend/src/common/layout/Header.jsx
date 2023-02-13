ccaa66ffe52dc2ded5ac420753a1a65f1792ea63
Enzo Rossi <enzorossi97@hotmail.com>
2023-02-13 09:56:49 -0300

    Adicionei o arquivo env apenas para deixar o site configurado e atualizei uns problemas no style

--- frontend/src/common/layout/Header.jsx
+++ frontend/src/common/layout/Header.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import Banner from "../banner/Banner";
 import BannerItem from "../banner/BannerItem";
 import CiaCarousel from "../carousel/CiaCarousel";
-
 import iconLocation from "../../res/icons/location.png";
+import "./Header.css";
+
 
 const Header = (props) => (
     <header className="header">
